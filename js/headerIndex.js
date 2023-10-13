025bb18adeb97dd720645b7d8e90fdc78e107d36
xdgtweb <110548730+xdgtweb@users.noreply.github.com>
2023-10-13 16:30:10 +0200

    x

---
+++ js/headerIndex.js
@@ -0,0 +1,8 @@
+const headerHTML = `
+    <img id="logo" src="./img/header/logo.png">
+    <nav>
+      <button id="btnInicio" onclick="cambiarScript('inicio')"><img src="./img/header/tornillo.svg">Nuevo</button>
+      <button id="btnCarta" onclick="cambiarScript('carta')"><img src="./img/header/diseno-fluido.svg">Linea</button>
+      <button id="btnContacto" onclick="cambiarScript('contacto')"><img src="./img/header/informacion-del-circulo-de-archivos.svg">Detalles</button>
+    </nav>
+`;
---
+++ js/mainIndex.js
@@ -0,0 +1,44 @@
+const mainIndexHTML = `
+
+  <div class="centrado" id="centradoF">
+    <video id="miVideo" autoplay muted loop reverse>
+      <source class="fondo" src="./img/index/banner.mp4" type="video/mp4">
+    </video>
+  </div>
+  <div class="centrado">
+    <label>Nuevo<label>
+  </div>
+  <div class="centrado">
+      <button onclick="cambiarScript('temporada1')">
+        <img src="./img/index/nuevo/1.jpg">
+        <label>Camiseta 1</label>
+      </button>
+      <button onclick="cambiarScript('temporada2')">
+        <img src="./img/index/nuevo/1.jpg">
+        <label>Camiseta 2</label>
+      </button>
+      <button onclick="cambiarScript('temporada3')">
+        <img src="./img/index/nuevo/1.jpg">
+        <label>Camiseta 3</label>
+      </button>
+      <button onclick="cambiarScript('temporada4')">
+        <img src="./img/index/nuevo/1.jpg">
+        <label>Camiseta 4</label>
+      </button>
+      <button onclick="cambiarScript('temporada5')">
+        <img src="./img/index/nuevo/1.jpg">
+        <label>Camiseta 5</label>
+      </button>
+      <button onclick="cambiarScript('temporada6')">
+        <img src="./img/index/nuevo/1.jpg">
+        <label>Camiseta 6</label>
+      </button>
+      <button onclick="cambiarScript('temporada7')">
+        <img src="./img/index/nuevo/1.jpg">
+        <label>Camiseta 7</label>
+      </button>
+    </div>
+  </div>
+
+
+`;
