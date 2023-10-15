e330d93a210dbcb2692d13790d7995f89dec55e8
xdgtweb <110548730+xdgtweb@users.noreply.github.com>
2023-10-15 22:35:01 +0200

    x

--- js/headerIndex.js
+++ js/headerIndex.js
@@ -4,6 +4,11 @@ const headerHTML = `
       <button onclick="cambiarPagina('index')"><img src="./img/header/tornillo.svg">Nuevo</button>
       <button onclick="cambiarPagina('lineas')"><img src="./img/header/diseno-fluido.svg">Lineas</button>
       <button onclick="cambiarPagina('detalles')"><img src="./img/header/informacion-del-circulo-de-archivos.svg">Detalles</button>
+      <div id="cajaUsuario">
+        <button><img src="./img/header/ingresar.svg">Login</button>
+        <img src="./img/header/logo.png">
+        <button><img src="./img/header/carrito.svg">Carrito</button>
+      </div>
     </nav>
     
 `;
