eb18ecc7ac7722618dd45f204e19cbf77c5e13c1
xdgtweb <110548730+xdgtweb@users.noreply.github.com>
2023-10-15 21:25:56 +0200

    x

---
+++ js-code/cambiarPagina.js
@@ -0,0 +1,31 @@
+function cambiarPagina(pagina) {
+    // Obtén la ruta actual de la página
+    var rutaActual = window.location.pathname;
+
+    switch (pagina) {
+        case 'index':
+            if (rutaActual.includes('/index.html')) {
+                // Ya estás en una página que contiene 'index.html', no hagas nada
+            } else {
+                window.location.href = './index.html';
+            }
+            break;
+        case 'lineas':
+            if (rutaActual.includes('/lineas.html')) {
+                // Ya estás en una página que contiene 'lineas.html', no hagas nada
+            } else {
+                window.location.href = './lineas.html';
+            }
+            break;
+        case 'detalles':
+            if (rutaActual.includes('/detalles.html')) {
+                // Ya estás en una página que contiene 'detalles.html', no hagas nada
+            } else {
+                window.location.href = './detalles.html';
+            }
+            break;
+        // Agregar más casos para otras páginas
+        default:
+            // Página no encontrada o manejar otros casos
+    }
+}
--- js/headerIndex.js
+++ js/headerIndex.js
@@ -1,8 +1,9 @@
 const headerHTML = `
     <img id="logo" src="./img/header/logo.png">
     <nav>
-      <button id="btnInicio" onclick="cambiarScript('inicio')"><img src="./img/header/tornillo.svg">Nuevo</button>
-      <button id="btnCarta" onclick="cambiarScript('carta')"><img src="./img/header/diseno-fluido.svg">Linea</button>
-      <button id="btnContacto" onclick="cambiarScript('contacto')"><img src="./img/header/informacion-del-circulo-de-archivos.svg">Detalles</button>
+      <button onclick="cambiarPagina('index')"><img src="./img/header/tornillo.svg">Nuevo</button>
+      <button onclick="cambiarPagina('lineas')"><img src="./img/header/diseno-fluido.svg">Lineas</button>
+      <button onclick="cambiarPagina('detalles')"><img src="./img/header/informacion-del-circulo-de-archivos.svg">Detalles</button>
     </nav>
+    
 `;
