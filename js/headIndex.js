9f133737e18f8b79b5d8eff933938ba33ece21e5
xdgtweb <110548730+xdgtweb@users.noreply.github.com>
2023-10-17 15:23:47 +0200

    x

--- js/headIndex.js
+++ js/headIndex.js
@@ -4,4 +4,5 @@ const headHTML = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="./img/header/logoBlanco.png" type="image/x-icon">
     <link rel="stylesheet" href="css/estilos1.css">
+
 `;
--- js/headerIndex.js
+++ js/headerIndex.js
@@ -1,12 +1,12 @@
 const headerHTML = `
-    <img id="logo" src="./img/header/logo.png">
+    <img id="logo" src="./img/header/logo.svg">
     <nav>
       <button onclick="cambiarPagina('index')"><img src="./img/header/tornillo.svg">Nuevo</button>
       <button onclick="cambiarPagina('lineas')"><img src="./img/header/diseno-fluido.svg">Lineas</button>
       <button onclick="cambiarPagina('detalles')"><img src="./img/header/informacion-del-circulo-de-archivos.svg">Detalles</button>
       <div id="cajaUsuario">
         <button onclick="cambiarPagina('login')"><img src="./img/header/ingresar.svg">Login</button>
-        <img src="./img/header/logo.png">
+        <img src="./img/header/logo.svg">
         <button onclick="cambiarPagina('cesta')"><img src="./img/header/cesta.svg">Cesta</button>
       </div>
     </nav>
